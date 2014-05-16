21604ecea430f1709174f8e3b3e471110e548378
Sebastian Wiemer <snwiem@googlemail.com>
2014-05-16 21:52:07 +0200

    added configurable webapp base

---
+++ webapp/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+import os
+
+instance = os.getenv('PY_COLLECTION_ENV', 'development')
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('webapp.config')
+app.config.from_pyfile('{0}.cfg'.format(instance), silent=False)
