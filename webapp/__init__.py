76fec6651c7defe8522fc738fee9311f0d9d15dc
Sebastian Wiemer <snwiem@googlemail.com>
2014-05-16 22:05:13 +0200

    added boostrap 101 and font awesome

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,3 +6,5 @@ instance = os.getenv('PY_COLLECTION_ENV', 'development')
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('webapp.config')
 app.config.from_pyfile('{0}.cfg'.format(instance), silent=False)
+
+import routes
